381fb462f3de46b7a61a913275bc4439f4e0bc0d
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2016-12-21 10:20:00 +0100

    Map vega.boardinghistory domain to persistence layer (#38)

--- backend-history-boarding/src/main/java/eu/socialedge/vega/backend/history/boarding/domain/Boarding.java
+++ backend-history-boarding/src/main/java/eu/socialedge/vega/backend/history/boarding/domain/Boarding.java
@@ -14,33 +14,49 @@
  */
 package eu.socialedge.vega.backend.history.boarding.domain;
 
-import eu.socialedge.vega.backend.ddd.AggregateRoot;
-import eu.socialedge.vega.backend.boarding.history.domain.BoardingId;
 import eu.socialedge.vega.backend.account.domain.PassengerId;
+import eu.socialedge.vega.backend.boarding.history.domain.BoardingId;
+import eu.socialedge.vega.backend.ddd.AggregateRoot;
 import eu.socialedge.vega.backend.terminal.domain.TerminalId;
 import eu.socialedge.vega.backend.transit.domain.location.Location;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import static org.apache.commons.lang3.Validate.notNull;
 
-@ToString
 @Getter
+@ToString
 @EqualsAndHashCode(callSuper = false)
+@Entity
+@NoArgsConstructor(force = true, access = AccessLevel.PACKAGE)
 public class Boarding extends AggregateRoot<BoardingId> {
 
+    @Column(nullable = false)
     private final BoardingType boardingType;
 
+    @Embedded
+    @Column(nullable = false)
     private final PassengerId passengerId;
 
+    @Embedded
+    @Column(nullable = false)
     private final TerminalId terminalId;
 
+    @Embedded
+    @Column(nullable = false)
     private final Location location;
 
+    @Column(nullable = false)
     private final LocalDateTime timestamp;
 
     public Boarding(BoardingId id, BoardingType boardingType, PassengerId passengerId,
