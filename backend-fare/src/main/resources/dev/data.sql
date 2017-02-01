c5db1c6574bb6c3d45b85ecd314089475cd4948a
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-02-01 08:48:35 +0100

    Enable preset in-memory database for dev profile in all microservices (#87)

---
+++ backend-fare/src/main/resources/dev/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO FARE
+(FARE_ID, PRICE, VALIDITY, POLY_PATH)
+VALUES
+(1, 'EUR 25.25', 'P1Y1M1D', '[{"latitude":0.0,"longitude":0.0},{"latitude":0.0,"longitude":11.0},{"latitude":11.0,"longitude":0.0},{"latitude":11.0,"longitude":11.0}]');
+
+INSERT INTO FARE
+(FARE_ID, PRICE, VALIDITY, POLY_PATH)
+VALUES
+(2, 'EUR 12.11', 'P1Y1M1D', '[{"latitude":0.0,"longitude":0.0},{"latitude":0.0,"longitude":22.0},{"latitude":22.0,"longitude":0.0},{"latitude":22.0,"longitude":22.0}]');
+
+INSERT INTO FARE_VEHICLE_TYPE
+(FARE_ID, VEHICLE_TYPES)
+VALUES
+(1, 'BUS');
+
+INSERT INTO FARE_VEHICLE_TYPE
+(FARE_ID, VEHICLE_TYPES)
+VALUES
+(2, 'TRAM');
+
+INSERT INTO FARE_OPERATOR
+(FARE_ID, OPERATOR_ID)
+VALUES
+(1, 1);
+
+INSERT INTO FARE_OPERATOR
+(FARE_ID, OPERATOR_ID)
+VALUES
+(2, 2);
+
+
